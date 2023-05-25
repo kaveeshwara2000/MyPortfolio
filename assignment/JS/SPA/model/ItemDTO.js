06aeca5e57639489b16aba1fc3d8bddc16adf54a
kaveeshwara2000 <sachindukaveeshwara@gmail.com>
2023-05-25 20:29:53 +0530

    DOM manipulation

---
+++ assignment/JS/SPA/model/ItemDTO.js
@@ -0,0 +1,8 @@
+class ItemDTO{
+    constructor(itemCode, itemName, qty, unitPrice) {
+        this.code = itemCode;
+        this.name = itemName;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+}
---
+++ assignment/JS/SPA/model/OrderDTO.js
@@ -0,0 +1,9 @@
+class OrderDTO{
+    constructor(oID, oDate, cusID, dis, total) {
+        this.oID = oID;
+        this.oDate = oDate;
+        this.cusID = cusID;
+        this.dis = dis;
+        this.total = total;
+    }
+}
